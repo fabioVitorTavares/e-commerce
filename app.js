aac49751c313ca6d29004d8c28aeb754886d31cc
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-10-28 17:24:24 -0300

    Correct response received

--- app.js
+++ app.js
@@ -1,104 +1,24 @@
 const express = require("express");
-const { randomUUID } = require("crypto");
-
-const fs = require("fs")
-
+const cors = require('cors')
 const app = express();
-
-app.use(express.urlencoded());
-
-app.use("/",express.static(__dirname));
+app.use(cors());
+app.use(express.json())
 
 
-
-const products = [];
-
-app.post("/addProduct", (request, response) =>{
-
-    
-
-    const { name, price } = request.body;
-
-    const product = {
-        name,
-        price
+const products = [
+    {
+        name : "car",
+        price: 1000.00
+    },
+    {
+        name: "phone",
+        price: 5000.00
     }
-
-    fs.writeFile("products.json", JSON.stringify(product), err =>{
-        if(err){
-            console.log(err);            
-        }
-        else{
-            console.log("Produto inserido");            
-        }
-    });
-
-    console.log(request.body);
-
-    
-    
-    // const {name, price} = request.body;
-
-    // const product = {
-    //     name,
-    //     price,
-    //     id: randomUUID(),
-    // }  
-    
-    // products.push(product);
-
-    
-    // return response.json({
-    //     message : "Hello world node.js and express framework and nodemon lib reload server" ,
-    // });
-    
-});
-
-app.get("/get", (request, response) => {
-    return response.json({
-        message : "Hello world node.js and express framework and nodemon lib reload server" ,
-    });
-});
-
-app.get("/products", (request, response) => {
-    return response.json(products);
-});
-
-app.get("/products/:id", (request, response) => {
-    const {id} = request.params;
-    const product = products.find(product => product.id === id);
-    return response.json(product);
-}); 
+];
 
 
-app.put("/products/:id", (request, response) => {
-    const {id} = request.params;
-
-    const { name, price } = request.body;    
-    
-    
-    const index = products.findIndex(product => product.id === id);
-    products[index] = {
-        ...products[index],
-        name,
-        price
-    }    
-    
-    return response.json({message: "Alterado com sucesso"});
-    
-    
-});
-
-app.delete("/products/:id", (request, response) => {
-    const {id} = request.params;   
-    
-    const index = products.findIndex(product => product.id === id);
-    
-    products.splice(index, 1);
-    
-    return response.json({message: "Deletado com sucesso"});
-    
-    
+app.post("/", (req, res) =>{      
+    res.json(products);    
 });
 
 
@@ -106,3 +26,5 @@ app.delete("/products/:id", (request, response) => {
 app.listen(4002, () =>{
     console.log("Server rodando na porta 4002");
 });
+
+
--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const getProducts = document.querySelector("#get-products");
+ const buttonSearch = document.querySelector("#buttonSearch");
+const url = "http://127.0.0.1:4002/";
 
-getProducts.addEventListener("click", () => {
-    document.location.href = "http://localhost:4002/get";
+const product = {
+    name: "food",
+    price: 8.50
+};
+
+
+const init = {   
+    method: 'POST',
+    body: JSON.stringify(product),
+    headers: {"Content-type": "application/json"}
+}
+
+
+
+buttonSearch.addEventListener('click', async () => {
+    const response = await fetch(url, init);
+    const datas = await response.json();
+    const datasJson = JSON.stringify(datas);
+    console.log(datas[0].name);    
 });
