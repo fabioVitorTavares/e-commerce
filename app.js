2f5a0b363ebabb268dcdcc30db1a3dc4fdf4737f
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-08 17:11:09 -0300

    commit

--- app.js
+++ app.js
@@ -14,33 +14,45 @@ const products = [{
     description: "Notebook lenovo i5",
     category: "Notebook",
     brand: "Lenovo",
-    price: 2459.50    
+    price: 2459.50
 },
 {
     img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
     description: "Notebook Dell i7",
     category: "Notebook",
     brand: "Dell",
-    price: 1000.00    
+    price: 1000.0    
 },
 {
     img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
     description: "Notebook Sansung i5",
     category: "Notebook",
     brand: "Sansung",
-    price: 3000.00    
+    price: 3000.1    
 },
 {
     img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
     description: "IPhone 13",
     category: "Smartphone",
     brand: "Aple",
-    price: 12000.00    
+    price: 12000.01    
 }];
 
 
-app.get("/products", (request, response) => {            
-    return response.json(products);
+app.get("/products", (request, response) => {
+    
+    const search = request.query.search; 
+    
+    
+    console.log(products.filter(element => {
+        console.log(element.description, search);
+        console.log(
+            element.description.includes(search)
+        );
+        
+    }));
+    
+    return response.json(products.filter(element => element.description.includes(search)));
 }); 
 
 app.post("/", (request, response) => {
--- script.js
+++ script.js
@@ -10,20 +10,19 @@ const url = "http://127.0.0.1:4002/";
 
 
 
-const content = document.querySelector('.content');
 
-buttonSearch.addEventListener('click',( async () => {
-    const response = await fetch(url+'products', {
+const functionSearch = async () => {
+  
+  content.innerHTML = "";
+  const response = await fetch(url+`products/?search=${inputSearch.value}`, {
         method: 'get',      
         headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        
+        'Content-Type': 'application/json',        
       }      
     });
     const data = await response.json();
-  
-    console.log(data);
+   
     data.forEach(element => {
       const classProduct = document.createElement('div');
       const classViewProduct = document.createElement('div');
@@ -35,34 +34,31 @@ buttonSearch.addEventListener('click',( async () => {
       classViewProduct.setAttribute('class', 'view');
       classDescriptionProduct.setAttribute('class', 'description');
       imgProduct.setAttribute('src', `${element.img}`);
-      descriptionProduct.innerText = element.description;
-      
-      
-      const formatedPrice = String(data.price).includes(".") ? String(data.price).replace(".",",") : String(data.price) + ",00";
-
-      priceProduct.innerHTML = `<p><strong>R$</strong> ${formatedPrice} </p>`;
-      console.log(formatedPrice);
-      
+      descriptionProduct.innerText = element.description;            
+      const regEx = /,\d$/;
+      let formatedPrice = String(element.price).includes(".") ? String(element.price).replace(".",",") : String(element.price) + ",00";
+      formatedPrice = regEx.test(formatedPrice) ? formatedPrice + "0" : formatedPrice;            
+      priceProduct.innerHTML = `<p><strong>R$</strong> ${formatedPrice} </p>`;      
       classProduct.appendChild(classViewProduct);
       classProduct.appendChild(classDescriptionProduct);
       classViewProduct.appendChild(imgProduct);
       classDescriptionProduct.appendChild(descriptionProduct);  
       classDescriptionProduct.appendChild(priceProduct);     
       content.appendChild(classProduct);      
-  });
+    });
+};
 
-}));
-
-
-
-   /*  const datas = await response.json();
-    
-    
-     */
+  const content = document.querySelector('.content');
+  buttonSearch.addEventListener('click', functionSearch);
 
 
-inputSearch.addEventListener("input", () => {   
-    
+document.addEventListener('keydown', (event) => {
+  if(event.key == "Enter"){
+    functionSearch();
+  }
+})
+
+inputSearch.addEventListener("input", () => {      
     buttonClear.style = inputSearch.value === "" ? "Display: none" : "Display: block";
 });
 
