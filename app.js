2f8bfbb7e285592fd84650ddaeba4ca80fb2e9c1
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-11-09 17:20:01 -0300

    add products in project

--- app.js
+++ app.js
@@ -10,57 +10,159 @@ app.use("/",express.static(__dirname));
 
 
 const products = [{
-    img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
-    description: "Notebook lenovo i5",
-    category: "Notebook",
-    brand: "Lenovo",
-    price: 2459.50
-},
-{
-    img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
-    description: "Notebook Dell i7",
+    img: "https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description: "Notebook Dell i5 8Gb SSD 128Gb",
     category: "Notebook",
     brand: "Dell",
-    price: 1000.0    
+    price: 3500.00
 },
 {
-    img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
-    description: "Notebook Sansung i5",
+    img: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description: "MacBook AMD chipset",
+    category: "Notebook",
+    brand: "Apple",
+    price: 8999.99    
+},
+{
+    img: "https://grupomassa.vtexassets.com/arquivos/ids/328109-800-auto?v=638018066876370000&width=800&height=auto&aspect=true",
+    description: "Notebook Sansung",
     category: "Notebook",
     brand: "Sansung",
-    price: 3000.1    
+    price: 3000.00    
 },
 {
-    img: "https://cdn.pixabay.com/photo/2016/10/12/13/32/office-1734485_960_720.jpg",
-    description: "IPhone 13",
+    img: "https://a-static.mlcdn.com.br/800x560/apple-iphone-13-pro-max-256gb-verde-alpino-67-12mp-ios/magazineluiza/234682100/76b6fcf3365e71e740fc94eb09407a3f.jpg",
+    description: "IPhone 13 Verde",
     category: "Smartphone",
-    brand: "Aple",
-    price: 12000.01    
+    brand: "Apple",
+    price: 12000.00    
+},
+{
+    img: "https://www.mobiledokan.com/wp-content/uploads/2020/01/Samsung-Galaxy-A71.jpg",
+    description: "Sansung A71",
+    category: "Smartphone",
+    brand: "Sansung",
+    price: 3000.00    
+},
+{
+    img: "https://www.notebookcheck.info/uploads/tx_nbc2/XiaomiRedmiNote9ProMax.JPG",
+    description: "Xiaomi Redmi Note 9 ProMax",
+    category: "Smartphone",
+    brand: "Xiaomi",
+    price: 4599.99    
+},
+{
+    img: "https://images-shoptime.b2w.io/produtos/6394749574/imagens/computador-pc-completo-intel-i7-memoria-ram-8gb-hd-1tb-wi-fi/6394749574_1_large.jpg",
+    description: "Computador de mesa Sansung completo",
+    category: "Desktop",
+    brand: "Sansung",
+    price: 2500.00    
+},
+{
+    img: "https://imgs.extra.com.br/1540996753/1xg.jpg",
+    description: "Desktop Dell XPS",
+    category: "Desktop",
+    brand: "Dell",
+    price: 2999.00    
+},
+{
+    img: "https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description: "Notebook Dell i5 8Gb SSD 128Gb",
+    category: "Notebook",
+    brand: "Dell",
+    price: 3500.00
+},
+{
+    img: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    description: "MacBook AMD chipset",
+    category: "Notebook",
+    brand: "Apple",
+    price: 8999.99    
+},
+{
+    img: "https://grupomassa.vtexassets.com/arquivos/ids/328109-800-auto?v=638018066876370000&width=800&height=auto&aspect=true",
+    description: "Notebook Sansung",
+    category: "Notebook",
+    brand: "Sansung",
+    price: 3000.00    
+},
+{
+    img: "https://a-static.mlcdn.com.br/800x560/apple-iphone-13-pro-max-256gb-verde-alpino-67-12mp-ios/magazineluiza/234682100/76b6fcf3365e71e740fc94eb09407a3f.jpg",
+    description: "IPhone 13 Verde",
+    category: "Smartphone",
+    brand: "Apple",
+    price: 12000.00    
+},
+{
+    img: "https://www.mobiledokan.com/wp-content/uploads/2020/01/Samsung-Galaxy-A71.jpg",
+    description: "Sansung A71",
+    category: "Smartphone",
+    brand: "Sansung",
+    price: 3000.00    
+},
+{
+    img: "https://www.notebookcheck.info/uploads/tx_nbc2/XiaomiRedmiNote9ProMax.JPG",
+    description: "Xiaomi Redmi Note 9 ProMax",
+    category: "Smartphone",
+    brand: "Xiaomi",
+    price: 4599.99    
+},
+{
+    img: "https://images-shoptime.b2w.io/produtos/6394749574/imagens/computador-pc-completo-intel-i7-memoria-ram-8gb-hd-1tb-wi-fi/6394749574_1_large.jpg",
+    description: "Computador de mesa Sansung completo",
+    category: "Desktop",
+    brand: "Sansung",
+    price: 2500.00    
+},
+{
+    img: "https://imgs.extra.com.br/1540996753/1xg.jpg",
+    description: "Desktop Dell XPS",
+    category: "Desktop",
+    brand: "Dell",
+    price: 2999.00    
 }];
 
 
 app.get("/products", (request, response) => {
     
-    const search = request.query.search; 
+    const search = JSON.parse(request.query.search);        
     
     
-    console.log(products.filter(element => {
-        console.log(element.description, search);
-        console.log(
-            element.description.includes(search)
-        );
-        
-    }));
     
-    return response.json(products.filter(element => element.description.includes(search)));
+    const searchByCategory = (element) => {
+        return !search.categorys.length ? true : search.categorys.includes(element.category);
+    }
+
+    const searchByBrand = (element) => {
+        return !search.brands.length ? true : search.brands.includes(element.brand);
+    }
+
+    const searchByMinPrice = (element) => {
+        return !search.minPrice ? true : element.price >= search.minPrice;
+    }
+    
+    const searchByMaxPrice = (element) => {
+        return !search.maxPrice ? true : element.price <= search.maxPrice;
+    }
+
+    
+    
+    console.log(products
+        .filter( element => element.description.toUpperCase().includes(search.search.toUpperCase()))
+        .filter(searchByCategory)     
+        .filter(searchByBrand)
+        .filter(searchByMinPrice)
+        .filter(searchByMaxPrice) 
+    );
+
+    return response.json(products
+    .filter( element => element.description.toUpperCase().includes(search.search.toUpperCase()))
+    .filter(searchByCategory)     
+    .filter(searchByBrand) 
+    .filter(searchByMinPrice)
+    .filter(searchByMaxPrice));   
+    
 }); 
-
-app.post("/", (request, response) => {
-    console.log(request.body);
-    response.json(request.body)    
-});
-
-
 
 
 
--- script.js
+++ script.js
@@ -1,28 +1,69 @@
+const url = "http://127.0.0.1:4002/";
 const buttonSearch = document.querySelector(".button-search");
 const inputSearch = document.querySelector('.input-search');
 const buttonClear = document.querySelector('.button-clear');
 
+const checkboxCategoryDesktop = document.querySelector('#checkboxCategoryDesktop'); 
+const checkboxCategoryNotebook = document.querySelector('#checkboxCategoryNotebook');
+const checkboxCategorySmartphone = document.querySelector('#checkboxCategorySmartphone');
+const checkboxBrandApple = document.querySelector('#checkboxBrandApple');
+const checkboxBrandDell = document.querySelector('#checkboxBrandDell');
+const checkboxBrandLenovo = document.querySelector('#checkboxBrandLenovo');
+const checkboxBrandSansung = document.querySelector('#checkboxBrandSansung');
+const checkboxBrandXiaomi = document.querySelector('#checkboxBrandXiaomi');
+const inputMinPrice = document.querySelector('#inputMinPrice');
+const inputMaxPrice = document.querySelector('#inputMaxPrice');
 
-const url = "http://127.0.0.1:4002/";
+
+const buttonClearFilterCategory = document.querySelector('#button-clear-filter-category');
+const buttonClearFilterBrand = document.querySelector('#button-clear-filter-brand');
+const buttonClearFilterPrice = document.querySelector('#button-clear-filter-price');
+const clearFilter = [buttonClearFilterCategory, buttonClearFilterBrand, buttonClearFilterPrice];
+
+const checksCategorys = [
+  checkboxCategoryDesktop,
+  checkboxCategoryNotebook,
+  checkboxCategorySmartphone
+];  
+
+const checksBrands = [
+  checkboxBrandApple,
+  checkboxBrandDell,
+  checkboxBrandLenovo,
+  checkboxBrandSansung,
+  checkboxBrandXiaomi
+];  
+
+function checkFilters() {
+  return  {
+    search: inputSearch.value,
+    categorys: checksCategorys.filter( element =>  element.checked).map(element => element.value),
+    brands: checksBrands.filter(element => element.checked).map(element => element.value),
+    minPrice: inputMinPrice.value.length > 0 ? inputMinPrice.value : null,
+    maxPrice: inputMaxPrice.value.length > 0 ? inputMaxPrice.value : null
+  }  
+};  
 
 
 
 
-
-
-
-const functionSearch = async () => {
+const search = async () => {
+  
+  const body = checkFilters();
+  
   
   content.innerHTML = "";
-  const response = await fetch(url+`products/?search=${inputSearch.value}`, {
-        method: 'get',      
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',        
-      }      
+  const response = await fetch(url+`products/?search=${JSON.stringify(body)}`, {
+    method: 'get',      
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',        
+    },  
+   
     });
     const data = await response.json();
-   
+    console.log(data);
+    
     data.forEach(element => {
       const classProduct = document.createElement('div');
       const classViewProduct = document.createElement('div');
@@ -45,27 +86,83 @@ const functionSearch = async () => {
       classDescriptionProduct.appendChild(descriptionProduct);  
       classDescriptionProduct.appendChild(priceProduct);     
       content.appendChild(classProduct);      
-    });
-};
-
+    });  
+  };  
+  
   const content = document.querySelector('.content');
-  buttonSearch.addEventListener('click', functionSearch);
+  buttonSearch.addEventListener('click', search);
 
+  
+checksCategorys.forEach(element => {       
+  element.addEventListener('change', search );
+})  
+
+checksCategorys.forEach(element => {       
+  element.addEventListener('change', () => {
+    const anyChecked = !!checksCategorys.find(element => element.checked);
+    buttonClearFilterCategory.style = anyChecked ? "Display: block" : "Display: none"; 
+  });
+}) 
+
+checksBrands.forEach(element => {
+  element.addEventListener('change', search );
+})  
+
+checksBrands.forEach(element => {       
+  element.addEventListener('change', () => {
+    const anyChecked = !!checksBrands.find(element => element.checked);
+    buttonClearFilterBrand.style = anyChecked ? "Display: block" : "Display: none"; 
+  });
+})
 
 document.addEventListener('keydown', (event) => {
   if(event.key == "Enter"){
-    functionSearch();
-  }
-})
+    search();
+  }  
+})  
 
 inputSearch.addEventListener("input", () => {      
     buttonClear.style = inputSearch.value === "" ? "Display: none" : "Display: block";
-});
+});    
 
 buttonClear.addEventListener("click", () => {
     inputSearch.value = "";
     buttonClear.style = inputSearch.value === "" ? "Display: none" : "Display: block";
     inputSearch.focus();
+});    
+
+const checksPriceEntry = () => {
+  return !(inputMinPrice.value.length == 0 && inputMaxPrice.value.length == 0);
+}
+
+inputMinPrice.addEventListener('input', () => {   
+  buttonClearFilterPrice.style = checksPriceEntry() ? "Display: block" : "Display: none";
+});
+
+inputMaxPrice.addEventListener('input', () => {
+  buttonClearFilterPrice.style = checksPriceEntry() ? "Display: block" : "Display: none";
 });
 
 
+buttonClearFilterCategory.addEventListener('click', () => {
+  checksCategorys.forEach(element => {
+    element.checked = false;
+  });
+  buttonClearFilterCategory.style = "Display: none";
+  search();
+});
+
+buttonClearFilterBrand.addEventListener('click', () => {
+  checksBrands.forEach(element => {
+    element.checked = false;
+  });
+  buttonClearFilterBrand.style = "Display: none";
+  search();
+});
+
+buttonClearFilterPrice.addEventListener('click', () => {
+  inputMinPrice.value = '';
+  inputMaxPrice.value = '';
+  buttonClearFilterPrice.style = "Display: none";
+  search();
+});
